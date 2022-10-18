a34aa4e6f72bfe8317308565dd2df0338f27dbd2
Alexey <vikulun_a97@mail.ru>
2022-10-18 12:47:42 +0700

    add example005

---
+++ Example/Example005/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Таня");
+Console.WriteLine("Ура, это же ЗАЙКА");
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
