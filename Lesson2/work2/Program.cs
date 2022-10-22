a3adf6a948189020ec840e0266e576ba2d4512ea
Alexey <vikulun_a97@mail.ru>
2022-10-22 15:38:33 +0700

    add work2

---
+++ Lesson2/work2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// 2. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+
+int number = new Random().Next(10, 100);
+
+int firstDigit = number / 10;
+int lastDigit = number % 10;
+if(firstDigit>lastDigit)
+
+     System.Console.WriteLine($"Первая цифра числа {number} большая {firstDigit}");
+     else
+     {
+          System.Console.WriteLine($"Первая цифра числа {number} меньшая {lastDigit}");
+
+     }
+
+
