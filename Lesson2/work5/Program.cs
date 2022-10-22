be5abeaac6b1cbeefb260889c43c06218b5f5395
Alexey <vikulun_a97@mail.ru>
2022-10-22 16:14:25 +0700

    add work5

---
+++ Lesson2/work5/Program.cs
@@ -0,0 +1,18 @@
+﻿// 2.1. Напишите программу, которая выводит случайное число из отрезка
+//  [10, 9999] и показывает наибольшую цифру числа.
+// 7812 -> 8
+// 1213-> 3
+// 845 -> 8
+
+
+int number = new Random().Next(10, 10000);
+System.Console.WriteLine(number);
+int maxdigit = number % 10;
+while (number > 0)
+{
+     int temp = number % 10;
+     if (number % 10 > maxdigit) { maxdigit = temp;}
+     number = number / 10;
+
+}
+System.Console.WriteLine(maxdigit);
