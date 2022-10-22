588381b8c8c2819f871e4c2929f724b9ffb5d00a
Alexey <vikulun_a97@mail.ru>
2022-10-22 12:25:38 +0700

    add exam3

---
+++ Example2/exam3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+int[] Array = { 1, 23, 74, 48, 5, 6, 74, 8, 9 };
+
+int n = Array.Length;
+int find =74;
+int index = 0;
+
+while( index < n)
+{
+     if(Array[index] == find)
+     {
+          System.Console.WriteLine(index);
+          break;
+     }
+
+
+     index++;
+}
