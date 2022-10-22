934b5a4833c02a68cd64d73dbdfc2d860d6b3cf2
Alexey <vikulun_a97@mail.ru>
2022-10-22 16:01:53 +0700

    add work4

---
+++ Lesson2/work4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+
+int Prompt(string message)
+{
+     System.Console.WriteLine(message);
+     int number = Convert.ToInt32(Console.ReadLine());
+     return number;
+}
+
+bool divBy(int number)
+{
+     return (number % 7 == 0) && (number % 23 == 0);
+}
+
+int A = Prompt("Введите число ");
+if (divBy(A))
+{
+     System.Console.WriteLine("Да");
+}
+else
+{
+     System.Console.WriteLine("Нет");
+}
