3c951b511a9efbb35d30e269f1151492ff8fb608
Alexey <vikulun_a97@mail.ru>
2022-11-10 11:02:12 +0700

    add work1

---
+++ Lesson3/work1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+
+int Promt(string massage)
+{
+     System.Console.Write(massage + " > ");
+     return int.Parse(Console.ReadLine());
+}
+
+bool ValidateNumber(int number)
+{
+     if (number <= 0)
+     {
+          System.Console.WriteLine("Число должно быть положительным");
+          return false;
+     }
+     return true;
+}
+
+int SumNumbers(int number)
+{
+     int sum = 0;
+     for(int i = 1; i <= number; i ++)
+     {
+          sum +=i;
+     }
+     return sum;
+}
+
+int number = Promt("Введите число");
+if(ValidateNumber(number))
+{
+     int sum = SumNumbers(number);
+     System.Console.WriteLine($"Сумма чисел от 1 до {number} равна {sum}");
+}
